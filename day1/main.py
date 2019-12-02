d7023e2f4840fe912108a2d548c038d6f5915239
ericlau00 <ericlau41802@gmail.com>
2019-12-02 03:33:58 -0500

    separate parts

--- day1/main.py
+++ day1/main.py
@@ -1,9 +1,12 @@
-def main():
+def main(part):
     masses = open("input.txt").read().split('\n')
     fuels = [int(mass) // 3 - 2 for mass in masses]
     total = 0
     for fuel in fuels:
-        total += sub(fuel)
+        if part == 1:
+            total += fuel
+        else:
+            total += sub(fuel)
     return total
 
 
@@ -15,4 +18,5 @@ def sub(fuel):
 
 
 if __name__ == "__main__":
-    print(main())
+    print("Answer to part 1:", main(1))
+    print("Answer to part 2:", main(2))
