880779d4203a02b8ca5cf4cad6669bc51a4f2efc
ericlau00 <ericlau41802@gmail.com>
2019-12-02 03:32:19 -0500

    complete day 1 with python

--- day1/main.py
+++ day1/main.py
@@ -1,10 +1,18 @@
 def main():
     masses = open("input.txt").read().split('\n')
-    fuels = [int(mass) // 3 - 2 for  mass in masses]
+    fuels = [int(mass) // 3 - 2 for mass in masses]
     total = 0
     for fuel in fuels:
-        total += fuel
-    print(total)
+        total += sub(fuel)
+    return total
+
+
+def sub(fuel):
+    if(fuel < 0):
+        return 0
+    else:
+        return sub(fuel // 3 - 2) + fuel
+
 
 if __name__ == "__main__":
-    main()
+    print(main())
