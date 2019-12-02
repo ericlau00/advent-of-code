2fb716e662bec81875a663e084030ecac0327b18
ericlau00 <ericlau41802@gmail.com>
2019-12-02 05:13:59 -0500

    add readme to part2, fix terminology

--- day2/main.py
+++ day2/main.py
@@ -1,18 +1,18 @@
-def intcode(seq):
-    seq[1] = 12
-    seq[2] = 2
-    opcode_pos = 0
-    while(seq[opcode_pos] != 99):
-        index1 = seq[opcode_pos + 1]
-        index2 = seq[opcode_pos + 2]
-        index3 = seq[opcode_pos + 3]
-        seq[index3] = \
-            seq[index1] + seq[index2] if seq[opcode_pos] == 1 \
-            else seq[index1] * seq[index2]
-        opcode_pos += 4
-    return seq
+def intcode(mem, noun, verb):
+    mem[1] = noun
+    mem[2] = verb
+    pointer = 0
+    while(mem[pointer] != 99):
+        param1 = mem[pointer + 1]
+        param2 = mem[pointer + 2]
+        param3 = mem[pointer + 3]
+        mem[param3] = \
+            mem[param1] + mem[param2] if mem[pointer] == 1 \
+            else mem[param1] * mem[param2]
+        pointer += 4
+    return mem
 
 
 if __name__ == "__main__":
     seq = [int(integer) for integer in open('input.txt').read().split(',')]
-    print("Answer to part 1:", intcode(seq)[0])
+    print("Answer to part 1:", intcode(seq, 12, 2)[0])
