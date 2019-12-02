f74b66476e0eaa782dd25af17bc8bebd7a21fcfc
ericlau00 <ericlau41802@gmail.com>
2019-12-02 13:45:51 -0500

    solve part 2 day 1 c

--- day1/main.c
+++ day1/main.c
@@ -7,25 +7,36 @@
 #include <sys/stat.h> //umask, stat
 #include <time.h>     //ctime
 
-int main()
-{
+int sub(int fuel) {
+    if (fuel < 0)
+        return 0;
+    else
+        return sub(fuel / 3 - 2) + fuel;
+}
+
+int solve(int part) {
     int SIZE = 100;
     int masses[SIZE];
-    FILE *file;
-
-    file = fopen("input.txt", "r");
+    FILE *file = fopen("input.txt", "r");
 
     int i;
-    int total;
-    for (i = 0; i < SIZE; i++)
-    {
+    int total = 0;
+    for (i = 0; i < SIZE; i++) {
         fscanf(file, "%d", &masses[i]);
-        total += masses[i] / 3 - 2;
+        int fuel = masses[i] / 3 - 2;
+        if (part == 1)
+            total += fuel;
+        else
+            total += sub(fuel);
     }
 
     fclose(file);
 
-    printf("Answer to part 1: %d\n", total);
+    return total;
+}
 
+int main() {
+    printf("Answer to part 1: %d\n", solve(1));
+    printf("Answer to part 2: %d\n", solve(2));
     return 0;
 }
