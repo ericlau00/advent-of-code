d392f70544c9404df0cf6c569a3b7af4dd258876
ericlau00 <ericlau41802@gmail.com>
2019-12-05 05:47:26 -0500

    solve day5 part 2

--- day5/main.py
+++ day5/main.py
@@ -1,4 +1,4 @@
-def intcode(mem, id):
+def intcode(mem, id, part):
     pointer = 0
     outputs = list()
     while(mem[pointer] != 99):
@@ -8,33 +8,56 @@ def intcode(mem, id):
             param_modes.insert(0, 0)
         param1 = mem[pointer + 1]
         param2 = mem[pointer + 2]
-        param3 = mem[pointer + 3]
+        if not opcode == 3 and not opcode == 4:
+            param3 = mem[pointer + 3]
 
-        if opcode == 1:
+        if opcode == 1 or opcode == 2:
             if not param_modes[-1]:
                 param1 = mem[param1]
             if not param_modes[-2]:
                 param2 = mem[param2]
-            mem[param3] = param1 + param2
+            mem[param3] = param1 + param2 if opcode == 1 \
+                else param1 * param2
             pointer += 4
-        elif opcode == 2:
-            if not param_modes[-1]:
-                param1 = mem[param1]
-            if not param_modes[-2]:
-                param2 = mem[param2]
-            mem[param3] = param1 * param2
-            pointer += 4
+            continue
         elif opcode == 3:
             mem[param1] = id
             pointer += 2
+            continue
         elif opcode == 4:
             if not param_modes[-1]:
                 param1 = mem[param1]
             outputs.append(param1)
             pointer += 2
-    return outputs
+            continue
+
+        if(part == 2):
+            if not param_modes[-1]:
+                param1 = mem[param1]
+            if not param_modes[-2]:
+                param2 = mem[param2]
+
+            if opcode == 5:
+                if(param1 != 0):
+                    pointer = param2
+                else:
+                    pointer += 3
+            elif opcode == 6:
+                if(param1 == 0):
+                    pointer = param2
+                else:
+                    pointer += 3
+            elif opcode == 7:
+                mem[param3] = 1 if param1 < param2 else 0
+                pointer +=4
+            elif opcode == 8:
+                mem[param3] = 1 if param1 == param2 else 0
+                pointer +=4
+    return outputs[-1]
 
 
 if __name__ == "__main__":
     seq = [int(integer) for integer in open('input.txt').read().split(',')]
-    print("Answer to part 1:", intcode(seq, 1))
+    print("Answer to part 1:", intcode(seq, 1, 1))
+    seq = [int(integer) for integer in open('input.txt').read().split(',')]
+    print("Answer to part 2:", intcode(seq, 5, 2))
