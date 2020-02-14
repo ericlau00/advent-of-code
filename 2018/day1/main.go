f95f7620b4aff21aef6a67f9c65b7de9aefd385a
ericlau00 <ericlau41802@gmail.com>
2020-02-14 12:00:30 -0500

    Change program organizatioon

--- 2018/day1/main.go
+++ 2018/day1/main.go
@@ -8,13 +8,7 @@ import (
 	"strings"
 )
 
-func main() {
-	input, err := ioutil.ReadFile("input.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	data := strings.Split(string(input), "\n")
+func getFrequencies(data []string) []int {
 	freqs := []int{}
 
 	for i := 0; i < len(data); i++ {
@@ -26,11 +20,32 @@ func main() {
 		freqs = append(freqs, num)
 	}
 
-	var sum int
+	return freqs
+}
+
+func solvePartOne(freqs []int) int {
+	sum := 0
 
 	for _, num := range freqs {
 		sum += num
 	}
 
-	fmt.Println("Answer to part 1:", sum)
+	return sum
+}
+
+func solvePartTwo(freqs []int) int {
+	return 0
+}
+
+func main() {
+	input, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data := strings.Split(string(input), "\n")
+	freqs := getFrequencies(data)
+
+	// fmt.Println("Answer to part 1:", solvePartOne(freqs))
+	fmt.Println("Answer to part 2:", solvePartTwo(freqs))
 }
